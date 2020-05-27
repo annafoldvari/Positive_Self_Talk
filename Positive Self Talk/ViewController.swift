69bce2772b4e1f18353f8481ed4d2f6001644136
anna_f <annafoldvari@gmx.co.uk>
2020-05-27 12:24:04 +0100

    Initial Commit

---
+++ Positive Self Talk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Positive Self Talk
+//
+//  Created by Anna Foldvari on 27/05/2020.
+//  Copyright © 2020 Anna Foldvari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
